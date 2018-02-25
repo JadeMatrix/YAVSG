08b4565af36ef23100293834d97f7f368f5dbd2c
Joseph Durel <jadematrix.art@gmail.com>
2018-02-24 19:33:18 -0500

    Figured out the depth buffer wasn't drawing because the depth texture mag & min filtering weren't set — improved framebuffer class is in the works

--- src/gl/framebuffer.cpp
+++ src/gl/framebuffer.cpp
@@ -135,6 +135,17 @@ namespace yavsg { namespace gl
                         + " for yavsg::gl::framebuffer"
                     );
                     
+                    glTexParameteri(
+                        GL_TEXTURE_2D,
+                        GL_TEXTURE_MIN_FILTER,
+                        GL_LINEAR
+                    );
+                    glTexParameteri(
+                        GL_TEXTURE_2D,
+                        GL_TEXTURE_MAG_FILTER,
+                        GL_LINEAR
+                    );
+                    
                     // TEXTURE:
                     glFramebufferTexture2D(
                         GL_FRAMEBUFFER,
