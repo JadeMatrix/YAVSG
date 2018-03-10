6d256f930e3a01d7a93f251a324256404b8283ca
Joseph Durel <jadematrix.art@gmail.com>
2018-03-10 01:27:31 -0500

    Added a basic texture "reference" class for lazy-loading textures

---
+++ include/rendering/texture_reference.hpp
@@ -0,0 +1,90 @@
+#pragma once
+#ifndef YAVSG_TEXTURE_REFERENCE_HPP
+#define YAVSG_TEXTURE_REFERENCE_HPP
+
+
+#include "../gl/texture.hpp"
+
+#include <string>
+
+
+namespace yavsg
+{
+    template<
+        typename DataType,
+        std::size_t Channels
+    > class texture_reference
+    {
+    public:
+        using texture_type = gl::texture< DataType, Channels >;
+        
+    protected:
+        texture_type*               _texture;
+        std::string                 _filename;
+        gl::texture_filter_settings _settings;
+        gl::texture_flags           _flags;
+        
+    public:
+        // This is very simple for now
+        texture_reference(
+            const std::string                & filename,
+            const gl::texture_filter_settings& settings,
+            gl::texture_flags                  flags = gl::texture_flags::NONE
+        );
+        
+        ~texture_reference();
+        
+        // These ensure the texture is created, so not strictly const in all
+        // situations
+        texture_type& texture();
+        const texture_type& texture() const;
+    };
+}
+
+
+namespace yavsg // Texture reference implementation ////////////////////////////
+{
+    template< typename DataType, std::size_t Channels >
+    texture_reference< DataType, Channels >::texture_reference(
+        const std::string                & filename,
+        const gl::texture_filter_settings& settings,
+        gl::texture_flags                  flags
+    ) :
+        _texture(  nullptr  ),
+        _filename( filename ),
+        _settings( settings ),
+        _flags(    flags    )
+    {}
+    
+    template< typename DataType, std::size_t Channels >
+    texture_reference< DataType, Channels >::~texture_reference()
+    {
+        if( _texture )
+            delete _texture;
+    }
+    
+    template< typename DataType, std::size_t Channels >
+    typename texture_reference< DataType, Channels >::texture_type&
+    texture_reference< DataType, Channels >::texture()
+    {
+        if( !_texture )
+            _texture = new texture_type( texture_type::from_file(
+                _filename,
+                _settings,
+                _flags
+            ) );
+        return *_texture;
+    }
+    
+    template< typename DataType, std::size_t Channels >
+    const typename texture_reference< DataType, Channels >::texture_type&
+    texture_reference< DataType, Channels >::texture() const
+    {
+        return const_cast< texture_reference< DataType, Channels >* >(
+            this
+        ) -> texture();
+    }
+}
+
+
+#endif
