bc2a1e72fa22b158b250fa7b841407482413067e
Joseph Durel <jadematrix.art@gmail.com>
2018-03-16 04:45:41 -0400

    First draft of a tasking system similar to jadebase's (untested)

---
+++ include/tasking/tasking.hpp
@@ -0,0 +1,63 @@
+#pragma once
+#ifndef YAVSG_TASKING_TASKING_HPP
+#define YAVSG_TASKING_TASKING_HPP
+
+
+#include <memory>   // std::unique_ptr
+#include <utility>  // std::size_t
+
+
+namespace yavsg // Tasks ///////////////////////////////////////////////////////
+{
+    using task_flags_type = std::size_t;
+    namespace task_flag
+    {
+        static const task_flags_type          NONE = 0x00;
+        
+        // Task must run on the main thread
+        static const task_flags_type   MAIN_THREAD = 0x01 << 0;
+        
+        // Task must run on the thread communicating with the GPU (usually but
+        // not necessarily the main thread)
+        static const task_flags_type    GPU_THREAD = 0x01 << 1;
+        
+        // Any thread that can execute this task should do so before any other
+        // task not also marked high priority
+        static const task_flags_type HIGH_PRIORITY = 0x01 << 2;
+    }
+    
+    class task
+    {
+    public:
+        virtual ~task();
+        virtual task_flags_type flags() const = 0;
+        virtual bool operator()() = 0;
+    };
+}
+
+
+namespace yavsg // Task system /////////////////////////////////////////////////
+{
+    void submit_task( std::unique_ptr< task > );
+    
+    using task_worker_flags_type = std::size_t;
+    namespace task_worker_flag
+    {
+        static const task_worker_flags_type        NONE = 0x00;
+        
+        // Thread is the main thread
+        static const task_worker_flags_type MAIN_THREAD = 0x01 << 0;
+        
+        // Thread is the/a thread communicating with the GPU
+        static const task_worker_flags_type  GPU_THREAD = 0x01 << 1;
+    }
+    
+    void become_task_worker( task_worker_flags_type = task_worker_flag::NONE );
+    
+    void initialize_task_system( bool main_will_task = true );
+    void initialize_task_system( std::size_t worker_count );
+    void       stop_task_system();
+}
+
+
+#endif
---
+++ src/tasking/tasking.cpp
@@ -0,0 +1,168 @@
+#include "../../include/tasking/tasking.hpp"
+
+#include <atomic>
+#include <condition_variable>
+#include <mutex>
+#include <thread>
+#include <vector>
+#include <list>
+
+
+namespace yavsg // Tasks ///////////////////////////////////////////////////////
+{
+    task::~task() {}
+}
+
+
+namespace // Task system globals ///////////////////////////////////////////////
+{
+    // Holds only the workers belonging to the tasking subsystem; there may be
+    // other threads running as workers via become_task_worker(), such as the
+    // main thread.
+    std::vector< std::thread > slaved_workers;
+    
+    // Subsystem & queues access control variables
+    std::atomic< bool > task_system_running{ false };
+    std::mutex queues_mutex;
+    std::condition_variable queues_condition;
+    
+    using task_ptr_type   = std::unique_ptr< yavsg::task >;
+    using task_queue_type = std::list< task_ptr_type >;
+    task_queue_type    main_hipri_queue;
+    task_queue_type    main_lopri_queue;
+    task_queue_type general_hipri_queue;
+    task_queue_type general_lopri_queue;
+}
+
+
+namespace yavsg // Task system /////////////////////////////////////////////////
+{
+    void submit_task( std::unique_ptr< task > t )
+    {
+        std::lock_guard< std::mutex > lock( queues_mutex );
+        
+        if(
+               t -> flags() & task_flag::MAIN_THREAD
+            || t -> flags() & task_flag:: GPU_THREAD
+        )
+        {
+            if( t -> flags() & task_flag::HIGH_PRIORITY )
+                main_hipri_queue.emplace_back( std::move( t ) );
+            else
+                main_lopri_queue.emplace_back( std::move( t ) );
+        }
+        else
+        {
+            if( t -> flags() & task_flag::HIGH_PRIORITY )
+                general_hipri_queue.emplace_back( std::move( t ) );
+            else
+                general_lopri_queue.emplace_back( std::move( t ) );
+        }
+        
+        queues_condition.notify_one();
+    }
+    
+    void become_task_worker( task_worker_flags_type worker_flags )
+    {
+        // Main thread and GPU thread are the same for now
+        if( worker_flags & task_worker_flag::MAIN_THREAD )
+            worker_flags |= task_worker_flag::GPU_THREAD;
+        
+        // List of queues to check, in order of priority
+        std::vector< task_queue_type* > queues;
+        
+        if( worker_flags & task_worker_flag::MAIN_THREAD )
+        {
+            queues.push_back( &main_hipri_queue );
+            queues.push_back( &main_lopri_queue );
+        }
+        queues.push_back( &general_hipri_queue );
+        queues.push_back( &general_lopri_queue );
+        
+        while( true )
+        {
+            std::unique_lock< std::mutex > lock( queues_mutex );
+            queues_condition.wait( lock );
+            
+            if( !task_system_running )
+            {
+                lock.unlock();
+                break;
+            }
+            
+            task_ptr_type task;
+            
+            // TODO: find an executable task & if found pop it
+            for( auto& queue : queues )
+                // for( auto& task : *queue )
+                for(
+                    auto task_iter = queue -> begin();
+                    task_iter != queue -> end();
+                    ++task_iter
+                )
+                {
+                    auto task_flags = ( *task_iter ) -> flags();
+                    
+                    if( (
+                        task_flags & task_flag::MAIN_THREAD
+                        && worker_flags & task_worker_flag::MAIN_THREAD
+                    ) || (
+                        task_flags & task_flag::GPU_THREAD
+                        && worker_flags & task_worker_flag::GPU_THREAD
+                    ) || task_flags & task_flag::NONE )
+                    {
+                        std::swap( task, *task_iter );
+                        queue -> erase( task_iter );
+                        goto done_searching;
+                    }
+                }
+        done_searching:
+            
+            // TODO: number of tasks in all queues
+            std::size_t total_tasks_enqueued = (
+                     main_hipri_queue.size()
+                +    main_lopri_queue.size()
+                + general_hipri_queue.size()
+                + general_lopri_queue.size()
+            );
+            
+            lock.unlock();
+            
+            if( total_tasks_enqueued > 0 )
+                // Let some other worker pop
+                queues_condition.notify_one();
+            
+            // TODO: execute task if found
+            if( task )
+            {
+                auto should_requeue = ( *task )();
+            }
+        }
+    }
+    
+    void initialize_task_system( bool main_will_task )
+    {
+        std::size_t num_workers = std::thread::hardware_concurrency();
+        if( main_will_task )
+            num_workers -= 1;
+        initialize_task_system( num_workers );
+    }
+    
+    void initialize_task_system( std::size_t worker_count )
+    {
+        task_system_running = true;
+        for( std::size_t i = 0; i < worker_count; ++i )
+            slaved_workers.emplace_back(
+                become_task_worker,
+                task_worker_flag::NONE
+            );
+    }
+    
+    void stop_task_system()
+    {
+        task_system_running = false;
+        queues_condition.notify_all();
+        for( auto& worker : slaved_workers )
+            worker.join();
+    }
+}
