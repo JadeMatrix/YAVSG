76029aa4b9c757bc225cadef8ca7f12ffc122eda
Joseph Durel <jadematrix.art@gmail.com>
2018-03-22 16:12:18 -0400

    `yavsg::callback_task` now uses `std::function` instead of a generic template parameter

--- include/tasking/utility_tasks.hpp
+++ include/tasking/utility_tasks.hpp
@@ -5,6 +5,8 @@
 
 #include "task.hpp"
 
+#include <functional>   // std::function
+
 
 namespace yavsg
 {
@@ -20,13 +22,17 @@ namespace yavsg
     
     // General-purpose callback-based task for lambdas etc; typically an
     // overload of `yavsg::task` should be used
-    template< class Callable > class callback_task : public task
+    class callback_task : public task
     {
-    protected:
-        Callable _callback;
-        task_flags_type _flags;
     public:
-        callback_task( Callable c, task_flags_type f ) :
+        using callback_type = std::function< bool( void ) >;
+        
+    protected:
+        callback_type _callback;
+        task_flags_type _flags;
+        
+    public:
+        callback_task( callback_type c, task_flags_type f ) :
             _callback( c ),
             _flags( f )
         {}
