9461fcd92e1d7df02886c724f0d0a475e87d901f
Joseph Durel <jadematrix.art@gmail.com>
2021-12-11 01:49:56 -0500

    Modernized code in `sdl` library

--- libraries/sdl/src/utils.cpp
+++ libraries/sdl/src/utils.cpp
@@ -4,49 +4,55 @@
 #include <SDL2/SDL_opengl.h>
 #include <SDL2/SDL_image.h>
 
-#include <exception>
-#include <string>
+#include <fmt/format.h>
+
+#include <stdexcept>    // runtime_error
+#include <string_view>
 
 
-namespace JadeMatrix::yavsg
+namespace
 {
-    SDL_manager::SDL_manager()
+    using namespace std::string_view_literals;
+}
+
+
+JadeMatrix::yavsg::SDL_manager::SDL_manager()
+{
+    if( SDL_Init( SDL_INIT_EVERYTHING ) != 0 )
     {
-        if( SDL_Init( SDL_INIT_EVERYTHING ) != 0 )
-            throw std::runtime_error(
-                "unable to initialize SDL2: "
-                + std::string( SDL_GetError() )
-            );
-        SDL_GL_SetAttribute( SDL_GL_CONTEXT_MAJOR_VERSION, 3                           );
-        SDL_GL_SetAttribute( SDL_GL_CONTEXT_MINOR_VERSION, 2                           );
-        SDL_GL_SetAttribute( SDL_GL_CONTEXT_PROFILE_MASK,  SDL_GL_CONTEXT_PROFILE_CORE );
-        SDL_GL_SetAttribute( SDL_GL_STENCIL_SIZE         , 8                           );
-        
-        int img_flags_in  = IMG_INIT_JPG | IMG_INIT_PNG | IMG_INIT_TIF;
-        int img_flags_out = IMG_Init( img_flags_in );
-        if( img_flags_in != img_flags_out )
-        {
-            std::string img_error_string =
-                "failed to initialize SDL2-image ";
-            if( img_flags_out & IMG_INIT_JPG )
-                img_error_string += "JPG";
-            else if( img_flags_out & IMG_INIT_PNG )
-                img_error_string += "PNG";
-            else if( img_flags_out & IMG_INIT_TIF )
-                img_error_string += "TIF";
-            img_error_string += (
-                " support: "
-                + std::string( IMG_GetError() )
-                + " (note IMG error string is not always meaningful when IMG_Init() fails)"
-            );
-            SDL_Quit();
-            throw std::runtime_error( img_error_string.c_str() );
-        }
+        throw std::runtime_error( fmt::format(
+            "unable to initialize SDL2: {}"sv,
+            SDL_GetError()
+        ) );
     }
+    SDL_GL_SetAttribute( SDL_GL_CONTEXT_MAJOR_VERSION, 3                           );
+    SDL_GL_SetAttribute( SDL_GL_CONTEXT_MINOR_VERSION, 2                           );
+    SDL_GL_SetAttribute( SDL_GL_CONTEXT_PROFILE_MASK,  SDL_GL_CONTEXT_PROFILE_CORE );
+    SDL_GL_SetAttribute( SDL_GL_STENCIL_SIZE         , 8                           );
     
-    SDL_manager::~SDL_manager()
+    auto const img_flags_in  = IMG_INIT_JPG | IMG_INIT_PNG | IMG_INIT_TIF;
+    auto const img_flags_out = IMG_Init( img_flags_in );
+    auto const img_flags_err = ( img_flags_in ^ img_flags_out ) & img_flags_in;
+    if( img_flags_err )
     {
-        IMG_Quit();
+        auto const img_error_string = fmt::format(
+            "failed to initialize SDL2-image {} support: {} (note IMG error "
+                "string is not always meaningful when IMG_Init() fails)"sv,
+            (
+                  img_flags_err & IMG_INIT_JPG ? "JPG"sv
+                : img_flags_err & IMG_INIT_PNG ? "PNG"sv
+                : img_flags_err & IMG_INIT_TIF ? "TIF"sv
+                : "?"sv
+            ),
+            IMG_GetError()
+        );
         SDL_Quit();
+        throw std::runtime_error( img_error_string );
     }
+}
+
+JadeMatrix::yavsg::SDL_manager::~SDL_manager()
+{
+    IMG_Quit();
+    SDL_Quit();
 }
