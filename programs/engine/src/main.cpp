8d4a44938d84d89aad64d7b8c20c3a98b4441dfa
Joseph Durel <jadematrix.art@gmail.com>
2021-12-19 12:30:50 -0500

    Modernized code in `engine` program

--- programs/engine/src/main.cpp
+++ programs/engine/src/main.cpp
@@ -16,17 +16,17 @@
 #include <yavsg/rendering/obj.hpp>
 #include <yavsg/units/angular.hpp>
 
+#include <cstddef>  // size_t
 #include <exception>
-#include <iostream> // std::clog
+#include <iostream> // clog
 #include <sstream>
 #include <string_view>
-#include <utility>  // std::size_t
 
 
 namespace
 {
-    const std::size_t window_width  = 1280;
-    const std::size_t window_height = 720;
+    constexpr std::size_t window_width  = 1280;
+    constexpr std::size_t window_height = 720;
     
     namespace yavsg = JadeMatrix::yavsg;
     
@@ -58,7 +58,7 @@ int main( int argc, char* argv[] )
         yavsg::SDL_manager sdl;
         
         yavsg::event_listener< SDL_QuitEvent > quit_listener{
-            []( const SDL_QuitEvent& ){
+            []( SDL_QuitEvent const& ){
                 log_.info( "Quitting..."sv );
                 yavsg::submit_task(
                     std::make_unique< yavsg::stop_task_system_task >()
@@ -91,15 +91,15 @@ int main( int argc, char* argv[] )
             true
         } );
         
-        auto desired_fov = yavsg::radians< GLfloat >{
+        auto const desired_fov = yavsg::radians< GLfloat >{
             yavsg::degrees< GLfloat >{ 90 }
         };
-        test_window -> main_scene.main_camera.fov( desired_fov );
-        test_window -> main_scene.main_camera.look_at( { 0.0f, 0.0f, 0.0f } );
-        test_window -> main_scene.main_camera.focal_point( 1.0f );
+        test_window->main_scene.main_camera.fov( desired_fov );
+        test_window->main_scene.main_camera.look_at( { 0.0f, 0.0f, 0.0f } );
+        test_window->main_scene.main_camera.focal_point( 1.0f );
         
         submit_task( std::make_unique< yavsg::load_obj_task >(
-            test_window -> main_scene.object_manager,
+            test_window->main_scene.object_manager,
             argv[ 1 ],
             argv[ 2 ]
         ) );
@@ -107,64 +107,72 @@ int main( int argc, char* argv[] )
         SDL_SetRelativeMouseMode( SDL_TRUE );
         
         yavsg::event_listener< SDL_MouseMotionEvent > camera_look_listener{
-            [ window_ref = test_window -> reference() ](
-                const SDL_MouseMotionEvent& e
+            [ window_ref = test_window->reference() ](
+                SDL_MouseMotionEvent const& e
             ){
-                std::unique_lock< std::mutex > window_reference_lock(
-                    window_ref -> reference_mutex
-                );
-                if( !window_ref -> window )
+                std::unique_lock wrlock( window_ref->reference_mutex );
+                if( !window_ref->window )
+                {
                     return;
+                }
                 
-                window_ref -> window -> main_scene.main_camera.increment_pitch_yaw(
+                window_ref->window->main_scene.main_camera.increment_pitch_yaw(
                     static_cast< float >( e.yrel ) / -4.0f,
                     static_cast< float >( e.xrel ) / -4.0f
                 );
             }
         };
         yavsg::event_listener< SDL_MouseWheelEvent > focal_adjust_listener{
-            [ window_ref = test_window -> reference() ](
-                const SDL_MouseWheelEvent& e
+            [ window_ref = test_window->reference() ](
+                SDL_MouseWheelEvent const& e
             ){
-                std::unique_lock< std::mutex > window_reference_lock(
-                    window_ref -> reference_mutex
-                );
-                if( !window_ref -> window )
+                std::unique_lock wrlock( window_ref->reference_mutex );
+                if( !window_ref->window )
+                {
                     return;
+                }
                 
                 auto new_focal_point = (
                     static_cast< float >( e.y )
                     / 4.0f
-                    + window_ref -> window -> main_scene.main_camera.focal_point()
+                    + window_ref->window->main_scene.main_camera.focal_point()
                 );
-                auto near = window_ref -> window -> main_scene.main_camera.near_point();
-                auto  far = window_ref -> window -> main_scene.main_camera. far_point();
+                auto near = window_ref->window->main_scene.main_camera.near_point();
+                auto  far = window_ref->window->main_scene.main_camera. far_point();
                 if( new_focal_point < near )
+                {
                     new_focal_point = near + 0.1f;
+                }
                 else if( new_focal_point > far )
+                {
                     new_focal_point = far - 0.1f;
-                window_ref -> window -> main_scene.main_camera.focal_point(
+                }
+                window_ref->window->main_scene.main_camera.focal_point(
                     new_focal_point
                 );
             }
         };
         yavsg::event_listener< SDL_KeyboardEvent > center_window_listener{
-            [ test_window ]( const SDL_KeyboardEvent& e ){
+            [ test_window ]( SDL_KeyboardEvent const& e ){
                 if( e.type == SDL_KEYUP && e.keysym.sym == SDLK_c )
                 {
                     log_.info( "Centering window..."sv );
-                    test_window -> center();
+                    test_window->center();
                 }
             }
         };
         yavsg::event_listener< SDL_KeyboardEvent > break_listener{
-            []( const SDL_KeyboardEvent& e ){
+            []( SDL_KeyboardEvent const& e ){
                 if( e.type == SDL_KEYUP && e.keysym.sym == SDLK_SPACE )
-                    std::clog << "\n--------------------------------------\n\n";
+                {
+                    std::clog
+                        << "\n--------------------------------------\n\n"sv
+                    ;
+                }
             }
         };
         yavsg::event_listener< SDL_KeyboardEvent > close_window_listener{
-            [ test_window ]( const SDL_KeyboardEvent& e ){
+            [ test_window ]( SDL_KeyboardEvent const& e ){
                 if( e.type == SDL_KEYUP && e.keysym.sym == SDLK_ESCAPE )
                 {
                     log_.info( "Closing window..."sv );
@@ -188,7 +196,7 @@ int main( int argc, char* argv[] )
         
         return 0;
     }
-    catch( const yavsg::gl::summary_error& e )
+    catch( yavsg::gl::summary_error const& e )
     {
         std::stringstream codes;
         yavsg::gl::print_summary_error_codes( codes, e );
@@ -198,7 +206,7 @@ int main( int argc, char* argv[] )
             codes.str()
         );
     }
-    catch( const std::exception& e )
+    catch( std::exception const& e )
     {
         log_.error( "Program exiting: {}"sv, e.what() );
     }
